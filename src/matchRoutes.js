41cc542d8bf9f4b30ae6532f32cf91091686b5a4
Gabe Meola <gmeola@overstock.com>
2018-03-08 11:57:07 -0700

    feat: updated matchRoute to return object of route and match

--- src/matchRoutes.js
+++ src/matchRoutes.js
@@ -5,16 +5,20 @@ export default function matchRoutes(config, pathname, shouldSwitch = true) {
     let matches = []
 
     for (let i = 0; i < config.length; i++) {
-        const isMatch = matchPath(pathname, config[i])
+        const route = config[i]
+        const match = matchPath(pathname, route)
 
-        if (isMatch) {
+        if (match) {
             // Look through subroutes first
-            if (config[i].routes) {
+            if (route.routes) {
                 // Add on Recursion of sub routes.
-                matches = matchRoutes(config[i].routes, pathname, false).concat(matches)
+                matches = matchRoutes(route.routes, pathname, false).concat(matches)
             }
 
-            matches.push(config[i])
+            matches.push({
+                route,
+                match,
+            })
 
             // Return out if we found a match in switch mode
             if (matches[0] && shouldSwitch) return matches[0]
