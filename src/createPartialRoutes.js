d5e35006268d4261a63ea3af99f3431878548e86
Gabe Meola <gmeola@overstock.com>
2018-03-08 11:51:17 -0700

    added new createPartialRoute to feature branch

--- src/createPartialRoutes.js
+++ src/createPartialRoutes.js
@@ -1,67 +1,33 @@
-import React from 'react'
 import Route from 'react-router/Route'
 import Redirect from 'react-router/Redirect'
 
 
-function createPartial(route) {
-    if (route.redirect == null) {
-        // return class extends Redirect {
-        //     static defaultProps = {
-        //         exact: route.exact,
-        //         strict: route.strict,
-        //         push: route.redirect.push,
-        //         from: route.redirect.from || route.path,
-        //         to: route.redirect.to,
-        //     }
-        // }
-
-        // eslint-disable-next-line no-inner-declarations
-        function PartialRedirect(props) {
-            const passProps = Object.assign({
+const createPartial = (route) =>
+    route.redirect == null
+        ? class PartialRoute extends Route {
+            static defaultProps = {
+                path: route.path,
+                exact: route.exact,
+                strict: route.strict,
+                component: route.component,
+            }
+        }
+        : class PartialRedirect extends Redirect {
+            static defaultProps = {
                 exact: route.exact,
                 strict: route.strict,
                 push: route.redirect.push,
                 from: route.redirect.from || route.path,
                 to: route.redirect.to,
-            }, props)
-
-            return React.createElement(Redirect, passProps)
-        }
-        PartialRedirect.propTypes = Redirect.propTypes
-
-        return PartialRedirect
-    }
-
-    function PartialRoute({ component: Comp, render: renderFunc, ...props }) {
-        return React.createElement(Route, Object.assign({}, props, {
-            render(props) {
-                const passProps = Object.assign({ routes: route.routes }, props)
-
-                return typeof renderFunc !== 'undefined'
-                    ? renderFunc(passProps)
-                    : React.createElement(Comp, passProps)
             }
-        }))
-    }
-    PartialRoute.defaultProps = {
-        path: route.path,
-        exact: route.exact,
-        strict: route.strict,
-        component: route.component,
-    }
-    PartialRoute.propTypes = Route.propTypes
-
-    return PartialRoute
-}
-
+        }
 
 export default function createPartialRoutes(routes) {
     let partialRoutes = {}
-    if (routes == null) return partialRoutes
 
     for (let i = 0; i < routes.length; i++) {
         const route = routes[i]
-        partialRoutes[route.name || route.path || route.redirect.from] = createPartial(route)
+        partialRoutes[route.name || route.path] = createPartial(route)
     }
 
     return partialRoutes
