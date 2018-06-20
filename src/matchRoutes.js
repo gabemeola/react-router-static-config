c93ba7810557a7fbae1b23ccb9c73f30db0daa39
Gabe M <gmeola@overstock.com>
2018-06-20 10:05:47 -0600

    fix: passing parent param to rr matchPath

--- src/matchRoutes.js
+++ src/matchRoutes.js
@@ -1,18 +1,19 @@
 import matchPath from 'react-router/matchPath'
 
 
-export default function matchRoutes(config, pathname, shouldSwitch = true) {
+export default function matchRoutes(config, pathname, shouldSwitch = true, parent) {
     let matches = []
 
     for (let i = 0; i < config.length; i++) {
         const route = config[i]
-        const match = matchPath(pathname, route)
+        // If we have no parent, fall back to the current route.
+        const match = matchPath(pathname, route, parent || route)
 
         if (match) {
             // Look through subroutes first
             if (route.routes) {
                 // Add on Recursion of sub routes.
-                matches = matchRoutes(route.routes, pathname, false).concat(matches)
+                matches = matchRoutes(route.routes, pathname, false, route).concat(matches)
             }
 
             matches.push({
